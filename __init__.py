397ad2b1d20beb4d5e6b0638570830a316d6b2ea
Frederick Lussier <frederuck.lussier@hotmail.com>
2017-09-30 11:56:52 -0400

    normalized to pep8

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+from .TinyPeriodicTask import TinyPeriodicTask
 
-__author__  = "Frederick Lussier <frederick.lussier@hotmail.com>"
-__status__  = "dev"
+__author__ = "Frederick Lussier <frederick.lussier@hotmail.com>"
+__status__ = "dev"
 __version__ = "0.1.0"
-__date__    = "august 28th 2017"
+__date__ = "august 28th 2017"
 
 __all__ = ["TinyPeriodicTask"]
-
-from .TinyPeriodicTask import TinyPeriodicTask
--- exemple.py
+++ exemple.py
@@ -1,10 +1,11 @@
-#import asyncio
 import time
 from TinyPeriodicTask import TinyPeriodicTask
 
-#The function to periodically run
+
 def task(message='this'):
-  print("I'm working on {0}".format(message))
+    # The function to periodically run
+    print("I'm working on {0}".format(message))
+
 
 task1 = TinyPeriodicTask(3, task)
 task1.start()
@@ -13,12 +14,12 @@ task2 = TinyPeriodicTask(6, task, message='that')
 task2.start()
 
 try:
-  print('Execution en cours')
+    print('Execution en cours')
 
-  while True:
-    time.sleep(0.5)
+    while True:
+        time.sleep(0.5)
 
 except KeyboardInterrupt:
-  task1.stop()
-  task2.stop()
-  print('Loop stopped')
+    task1.stop()
+    task2.stop()
+    print('Loop stopped')
