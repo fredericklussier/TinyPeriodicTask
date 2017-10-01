7c93845ffb7687c81d586ccb3ba69a9acbac6bb8
Frederick Lussier <frederuck.lussier@hotmail.com>
2017-09-30 21:29:19 -0400

    Set prod version

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 from .TinyPeriodicTask import TinyPeriodicTask
 
 __author__ = "Frederick Lussier <frederick.lussier@hotmail.com>"
-__status__ = "dev"
-__version__ = "0.1.0"
-__date__ = "august 28th 2017"
+__status__ = "prod"
+__version__ = "1.1.0"
+__date__ = "august 30th 2017"
 
 __all__ = ["TinyPeriodicTask"]
