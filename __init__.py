a0afb0ffc46a71eee790b74a83384fc4d28b7c41
Frederick Lussier <frederuck.lussier@hotmail.com>
2017-09-30 21:30:17 -0400

    fix date

--- __init__.py
+++ __init__.py
@@ -5,6 +5,6 @@ from .TinyPeriodicTask import TinyPeriodicTask
 __author__ = "Frederick Lussier <frederick.lussier@hotmail.com>"
 __status__ = "prod"
 __version__ = "1.1.0"
-__date__ = "august 30th 2017"
+__date__ = "september 30th 2017"
 
 __all__ = ["TinyPeriodicTask"]
